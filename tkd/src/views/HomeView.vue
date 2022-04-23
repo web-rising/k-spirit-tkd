4e8de7d4389604804f6d6d7786e58572a5a507db
heochristopher <71462915+heochristopher@users.noreply.github.com>
2022-04-23 16:30:16 -0400

    img

--- tkd/src/views/HomeView.vue
+++ tkd/src/views/HomeView.vue
@@ -1,8 +1,11 @@
 <template>
 	<div class="container">
 		<header class="block">
-			<h1>{{$prismic.asText(homepage.data.name)}}</h1>
+			<h1>hello</h1>
+			<!-- <h1>{{$prismic.asText(homepage.data.name)}}</h1> -->
 			<h2>{{$prismic.asText(homepage.data.slogan)}}</h2>
+			<!-- <div>{{$prismic.asImageSrc(homepage.data.image1)}}</div> -->
+			<prismic-image :field="homepage.data.image1"></prismic-image>
 		</header>
 	</div>
 </template>
