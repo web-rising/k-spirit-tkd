6231a1003c8014209e55baf7ea037621c7e2550b
heochristopher <71462915+heochristopher@users.noreply.github.com>
2022-04-23 16:42:05 -0400

    Update HomeView.vue

--- tkd/src/views/HomeView.vue
+++ tkd/src/views/HomeView.vue
@@ -3,9 +3,8 @@
 		<header class="block">
 			<h1>hello</h1>
 			<!-- <h1>{{$prismic.asText(homepage.data.name)}}</h1> -->
-			<h2>{{$prismic.asText(homepage.data.slogan)}}</h2>
 			<!-- <div>{{$prismic.asImageSrc(homepage.data.image1)}}</div> -->
-			<prismic-image :field="homepage.data.image1"></prismic-image>
+			<prismic-image :field="homepage.data.splash"></prismic-image>
 		</header>
 	</div>
 </template>
