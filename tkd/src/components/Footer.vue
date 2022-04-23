084e784afd78e6be549d6b1ab40cc1523f8cc105
jasonchen3805 <90630687+jchn3805@users.noreply.github.com>
2022-04-23 17:01:34 -0400

    x

---
+++ tkd/src/components/Footer.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="footer">
+        <div class="container">
+            <div class="contact">
+                <h5 class="email">{{$prismic.asText(contact.data.email)}}</h5>
+                <h5 class="telephone">{{$prismic.asText(contact.data.number)}}</h5>
+                <h5 class="address">{{$prismic.asText(contact.data.address)}}</h5>
+            </div>
+            <ul class="nav-list">
+                <li>
+                    <router-link to="/mission">Our Mission</router-link>
+                </li>
+                <li>
+                    <router-link to="/team">Our Team</router-link>
+                </li>
+                <li>
+                    <router-link to="/work">Our Work</router-link>                
+                </li>
+                <li>
+                    <a href="https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=webrisingdev@gmail.com">Contact Us</a>
+                </li>
+            </ul>
+            </div>
+        <h6 class="copyright">Copyright 2022 WebRising All Rights Reserved</h6>
+    </div>
+</template>
+
+<script setup>
+import { useSinglePrismicDocument } from "@prismicio/vue";
+
+const { data: contact } = useSinglePrismicDocument("contact");
+
+</script>
--- tkd/src/views/HomeView.vue
+++ tkd/src/views/HomeView.vue
@@ -1,11 +1,9 @@
 <template>
 	<div class="container">
-		<header class="block">
-			<h1>hello</h1>
-			<!-- <h1>{{$prismic.asText(homepage.data.name)}}</h1> -->
-			<!-- <div>{{$prismic.asImageSrc(homepage.data.image1)}}</div> -->
-			<prismic-image :field="homepage.data.splash"></prismic-image>
-		</header>
+		<h1>hello</h1>
+		<!-- <h1>{{$prismic.asText(homepage.data.name)}}</h1> -->
+		<!-- <div>{{$prismic.asImageSrc(homepage.data.image1)}}</div> -->
+		<prismic-image :field="homepage.data.splash"></prismic-image>
 	</div>
 </template>
 
