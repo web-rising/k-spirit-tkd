eaec63605850f045e79bb4d37e574e2593cbd0ac
heochristopher <71462915+heochristopher@users.noreply.github.com>
2022-04-23 15:02:49 -0400

    asdf

--- tkd/src/router/index.js
+++ tkd/src/router/index.js
@@ -7,7 +7,9 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: () => {
+        import('../views/HomeView.vue')
+      }
     },
     {
       path: '/about',
--- tkd/src/views/HomeView.vue
+++ tkd/src/views/HomeView.vue
@@ -1,9 +1,21 @@
-<script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
+<template>
+  <div>
+    <h1>yo</h1>
+   <h1>{{$prismic.asText(homepage.data.name)}}</h1>
+	<h2>{{$prismic.asText(homepage.data.slogan)}}</h2>
+  </div>
+</template>
+
+<script>
+import {useSinglePrismicDocument} from '@/prismicio/vue'
+
+const {data: homepage} = useSinglePrismicDocument('homepage')
+
+export default {
+ 
+}
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style>
+
+</style>
