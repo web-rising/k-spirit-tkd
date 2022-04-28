90f546faf3293f97fdd8f889d8815e3e1aed5701
jasonchen3805 <90630687+jchn3805@users.noreply.github.com>
2022-04-28 11:52:33 -0400

    x

--- tkd/src/views/HomeView.vue
+++ tkd/src/views/HomeView.vue
@@ -12,13 +12,18 @@ console.log(homepage)
 			<prismic-image :field="homepage.data.splash" class="flag"></prismic-image>
 
 			<div class="mask"></div>
-			<div class="text">
+			<div class="splash-text">
 				<h1>"Make the impossible possible."</h1>
 				<h4>K-Spirit Taekwondo</h4>
 			</div>
 		</section>
 		<section class="news">
 			<h2>News & Updates</h2>
+			<div class="news-container">
+				<div class="news-item"></div>
+				<div class="news-item"></div>
+				<div class="news-item"></div>
+			</div>
 		</section>
 		<section class="gallery">
 			<swiper
@@ -87,7 +92,19 @@ console.log(homepage)
 			</swiper>
 		</section>
 		<section class="meet">
-			<h2>Meet Master Cho</h2>
+			<h4>Meet Master Cho</h4>
+			<div class="profile"></div>
+		</section>
+		<section class="schedule">
+			<h2>Schedule</h2>
+			<div class="picture"></div>
+		</section>
+		<section class="visit">
+			<h4>Come visit our dojang!</h4>
+			<div class="location">
+				<img src="../assets/pin.svg" alt="" class="pin">
+				<h5>Bay Ridge, Brooklyn</h5>
+			</div>
 		</section>
 	</div>
 </template>
@@ -113,6 +130,7 @@ import "swiper/css/pagination";
 		flex-direction: column;
 		justify-content: center;
 		align-items: start;
+		color: #fff
 	}
 
 	.flag {
@@ -124,7 +142,7 @@ import "swiper/css/pagination";
 		z-index: -2;
 	}
 
-	.text {
+	.splash-text {
 		padding: 0 1rem;
 	}
 
@@ -137,25 +155,81 @@ import "swiper/css/pagination";
 	}
 
 	.news {
-		background-color: blue;
 		height: 40vh;
+		width: 100%;
 		display: flex;
 		flex-direction: column;
 		justify-content: center;
 		align-items: center;
 	}
+	.news-container {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.news-item {
+		margin: 0.25rem;
+		width: 23rem;
+		height: 3.5rem;
+		background-color: #151515;
+	}
 	.gallery {
 		width: 100%;
-		height: 60rem;
+		height: 60vh;
 		object-fit: cover;
-		z-index: 1;
+		display: flex;
+		justify-content: center;
+		background-color: #151515;
+		/* align-items: center; */
 	}
 
 	.image {
 		width: 100%;
 	}
 	.meet {
-		
+		height: 25vh;
+		width: 100%;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: space-around;
+	}
+	.profile {
+		width: 8rem;
+		height: 8rem;
+		border-radius: 4rem;
+		background-color: #151515;
+	}
+	.schedule {
+		height: 32vh;
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+	.picture {
+		width: 90%;
+		height: 15rem;
+		background-color: #151515;
+	}
+
+	.visit {
+		height: 30vh;
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.location {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: space-around;
 	}
 }
 </style>
