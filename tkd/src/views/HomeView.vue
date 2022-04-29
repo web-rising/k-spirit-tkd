4a6f73d02e5751662b830965c905eb7d6639f76f
Blanku-cloud <pokemondinner321@gmail.com>
2022-04-29 10:07:32 -0400

    Update HomeView.vue

--- tkd/src/views/HomeView.vue
+++ tkd/src/views/HomeView.vue
@@ -68,6 +68,25 @@ import { Swiper, SwiperSlide } from "swiper/vue";
 
 // Import Swiper styles
 import "swiper/css";
+
+export default {
+	components: {
+		Swiper,
+		SwiperSlide,
+	},
+	setup() {
+		const onSwiper = (swiper) => {
+			console.log(swiper);
+		};
+		const onSlideChange = () => {
+			console.log("slide change");
+		};
+		return {
+			onSwiper,
+			onSlideChange,
+		};
+	},
+};
 </script>
 
 <style scoped>
