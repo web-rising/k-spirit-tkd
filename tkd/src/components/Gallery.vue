c21af5b6959706cc39bf50993102b87a29cd8472
J0hns0n-Wang <johnsonwang529@gmail.com>
2022-05-29 21:51:52 -0400

    got the plain html for gallery in, need to do styling and hover effects tmr

---
+++ tkd/src/components/Gallery.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="gallery">
+    <div class="pic">
+      <!-- <img :src="require(`@/assets/clients/${image}`)" alt="" /> -->
+      <div class="mask">
+        <img :src="`${image}`" alt="" />
+        <h5>{{ description }}</h5>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Gallery",
+  data() {
+    return {};
+  },
+  methods: {},
+  props: {
+    image: String,
+    description: String,
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
--- tkd/src/views/GalleryView.vue
+++ tkd/src/views/GalleryView.vue
@@ -1,23 +1,32 @@
-<script setup>
-import { useSinglePrismicDocument } from "@prismicio/vue";
-
-const { data: about } = useSinglePrismicDocument("about");
-</script>
-
 <template>
+<div class="container">
   <h1>Gallery</h1>
+    <Gallery
+            v-for="(item, index) in images"
+            :key="index"
+            :image="item.image.url"
+            :description="item.description[0].text"
+          />
+          <!-- <p>{{images}}</p> -->
+          <!-- <li v-for="(item, index) in images" :key="index">{{item.image.url}}</li> -->
+</div>
+  
+  
 </template>
 
 <script>
 import limitImages from '../prismicHelpers/gallery'
+import Gallery from '../components/Gallery.vue'
 
 export default {
+  components:{
+    Gallery
+  },
   data() {
     return {
       images: limitImages(12)
     }
   }
-
 }
 </script>
 
