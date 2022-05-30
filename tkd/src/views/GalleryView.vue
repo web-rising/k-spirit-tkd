fe2ac17274959608a0d27869a1c866a8b931721f
J0hns0n-Wang <johnsonwang529@gmail.com>
2022-05-29 21:17:52 -0400

    fv

--- tkd/src/views/GalleryView.vue
+++ tkd/src/views/GalleryView.vue
@@ -1,3 +1,9 @@
+<script setup>
+import { useSinglePrismicDocument } from "@prismicio/vue";
+
+const { data: about } = useSinglePrismicDocument("about");
+</script>
+
 <template>
   <h1>Gallery</h1>
 </template>
