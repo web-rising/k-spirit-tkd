f49777e16cf6bb1c5eebe68e98ffa53dc9a97939
heochristopher <71462915+heochristopher@users.noreply.github.com>
2022-08-13 20:59:52 -0400

    Update Carousel.vue

--- tkd/components/Carousel.vue
+++ tkd/components/Carousel.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="carousel w-full h-72 md:h-80 lg:h-[75vh]">
-    <div v-for="(image, index) in images" :key="image.image.url" :id="`slide${index + 1}`" class="carousel-item relative w-full">
+  <div class="carousel carousel-center w-full h-72 md:h-80 lg:h-[75vh]">
+    <div v-for="(image, index) in images" :key="image.image.url" :id="`slide${index + 1}`" class="carousel-item relative w-5/6 md:w-1/2">
       <img :src="image.image.url" class="w-full object-cover" @click="$router.push('/gallery')" />
-      <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <!-- <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
         <a :href="`#slide${index === 0 ? images.length : index}`" class="btn btn-circle">❮</a>
         <a :href="`#slide${index === images.length - 1 ? 1 : index + 2}`" class="btn btn-circle">❯</a>
-      </div>
+      </div> -->
     </div>
   </div>
 </template>
